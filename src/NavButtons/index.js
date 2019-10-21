51a240cd07c4bfa52cf92fcd7692d670c9224c74
Aaron Weissman <weissman.aaron@gmail.com>
2019-10-21 06:18:09 -0600

    links and image stuff + readme

--- src/NavButtons/index.js
+++ src/NavButtons/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 // CSS
 import './NavButtons.css';
@@ -15,11 +16,11 @@ const NavButtons = (props) => {
         <input className="menu-btn" type="checkbox" id="menu-btn" />
         <label className="menu-icon" htmlFor="menu-btn"><span className="navicon"></span></label>
         <ul className="menu">
-          <li><a href="/schedule">Schedule</a></li>
-          <li><a href="/story">Our Story</a></li>
-          <li><a href="/logistics">Logistics</a></li>
-          <li><a href="/stuff">What to bring</a></li>
-          <li><a href="/todo">Things to do</a></li>
+          <li><Link to={{ pathname: "/schedule" }}>Schedule</Link></li>
+          <li><Link to={{ pathname: "/story" }}>Our Story</Link></li>
+          <li><Link to={{ pathname: "/logistics" }}>Logistics</Link></li>
+          <li><Link to={{ pathname: "/stuff" }}>What to bring</Link></li>
+          <li><Link to={{ pathname: "/todo" }}>Things to do</Link></li>
           <li><a href='https://www.rei.com/' target='_blank' rel="noopener noreferrer">Registry</a></li>
           <li><a href="https://docs.google.com/forms/d/e/1FAIpQLSdGHs8Szep3Kau7LPRj1DhhYNgDSUi0xtVSfcjOq8l3ibyy-A/viewform?usp=sf_link" target='_blank' rel="noopener noreferrer"><b>RSVP</b></a></li>
         </ul>
--- src/OtherInfo/index.js
+++ src/OtherInfo/index.js
@@ -2,17 +2,37 @@ import React from 'react';
 
 // CSS
 import './OtherInfo.css';
-import Ceremony from '../Ceremony';
 
 const OtherInfo = () => {
   return (
     <div className='other-info'>
-      <Ceremony />
+      <div className='ceremony-container'>
+        <h1 className='ceremony-title'>The Campground</h1>
+        <p className='ceremony-info'>The ceremony will be held on July 4th at <a href='https://www.google.com/maps/place/Windy+Point+Group+Campground/@39.6078137,-106.0454653,15z/data=!4m2!3m1!1s0x0:0xb5c6b7ae8c358e23?sa=X&ved=2ahUKEwjtnM_Aq5XlAhXMsJ4KHQmpA6kQ_BIwDHoECA0QCA' target='_blank' rel="noopener noreferrer">Windy Point Campground</a> in Dillon, CO</p>
+        <iframe
+          title='windy-point-map'
+          src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12295.443977324252!2d-106.0454653!3d39.6078137!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb5c6b7ae8c358e23!2sWindy%20Point%20Group%20Campground!5e0!3m2!1sen!2sus!4v1570987965673!5m2!1sen!2sus"
+          frameBorder="0"
+          className='windy-map'
+          style={{
+            border: 0,
+            height: '600px',
+            width: window.innerWidth < 768 ? '90%' : '75%',
+            borderRadius: '12px'
+          }}
+          allowFullScreen=""
+        >
+        </iframe>
+      </div>
+      <div className='circle-div'>
+        <div className='tent-pic'></div>
+      </div>
       <h1>Camping Resources</h1>
       <h3>Kit Rentals:</h3>
-      <p><a href='www.rei.com' target='_blank' rel="noopener noreferrer">REI</a></p>
-      <p><a href='www.mountainsidegearrental.com' target='_blank' rel="noopener noreferrer">Mountain Side Gear Rental</a></p>
-      <p><a href='www.vitaloutdoors.com' target='_blank' rel="noopener noreferrer">Vital Outdoors</a></p>
+      <p><a target='_blank' rel="noopener noreferrer" href='www.rei.com' >REI</a></p>
+      <p><a target='_blank' rel="noopener noreferrer" href='www.mountainsidegearrental.com' >Mountain Side Gear Rental</a></p>
+      <p><a target='_blank' rel="noopener noreferrer" href='www.vitaloutdoors.com' >Vital Outdoors</a></p>
+      <p><a target='_blank' rel="noopener noreferrer" href='https://www.outdoorsgeek.com/product-category/camping-packages/' >Outdoors Geek</a></p>
     </div>
   );
 }
