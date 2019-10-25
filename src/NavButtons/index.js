f1f10013a3e6d9a0f13235a321a554a48adf68be
Aaron Weissman <weissman.aaron@gmail.com>
2019-10-25 17:51:41 -0600

    better menu and stickied header in mobile

--- src/NavButtons/index.js
+++ src/NavButtons/index.js
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 // CSS
 import './NavButtons.css';
 
 const NavButtons = (props) => {
-  return (<>
+  const [imageClass, setImageClass] = useState('');
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+  }, [])
+  const handleScroll = (e) => {
+    // console.log('e:', window.scrollY);
+    if (window.scrollY > 300) {
+      setImageClass('fixed-header');
+    } else {
+      setImageClass('');
+    }
+  }
+
+  return (<div className={'whole-header'}>
     <div className='header-image'>
     </div>
-    <div className='nav-box'>
-      <header className='header high'>
-        <a href="/">
-          <div className='logo-icon'></div>
+    <div className={'nav-box ' + imageClass}>
+      <header className={'header'}>
+        <a className='logo-link' href="/">
+          <img
+            alt='logo'
+            className='logo-icon'
+            src={require('../assets/camping-icon-tpbg.jpg')}
+          />
         </a>
         <input className="menu-btn" type="checkbox" id="menu-btn" />
         <label className="menu-icon" htmlFor="menu-btn"><span className="navicon"></span></label>
@@ -26,7 +44,7 @@ const NavButtons = (props) => {
         </ul>
       </header>
     </div>
-  </>);
+  </div>);
 }
 
 export default NavButtons;
