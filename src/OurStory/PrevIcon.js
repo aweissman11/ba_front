ae032b6922a8d70a348c1a8a3199593a9ea3d8dd
Aaron Weissman <weissman.aaron@gmail.com>
2019-11-02 16:33:56 -0600

    happy slider

---
+++ src/OurStory/PrevIcon.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const PrevIcon = (props) => {
+  return (
+    <svg className='slide-btn prev' stroke='currentColor' fill='currentColor' strok-width='0' viewBox='0 0 24 24' height='1em' width='1em' xmlns='http://www.w3.org/2000/svg'>
+      <path d='M15.41 16.09l-4.58-4.59 4.58-4.59L14 5.5l-6 6 6 6z'></path>
+    </svg>
+  );
+}
--- src/OurStory/index.js
+++ src/OurStory/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import AwesomeSlider from 'react-awesome-slider';
 import AwesomeSliderStyles from 'react-awesome-slider/src/styled/scale-out-animation/scale-out-animation.scss';
 
@@ -8,15 +8,84 @@ import { hisStory, herStory } from './stories';
 
 // CSS
 import './OurStory.css';
+import { NextIcon } from './NextIcon';
+import { PrevIcon } from './PrevIcon';
 
 const OurStory = () => {
+  let [currentPosition, setPosition] = useState(0);
+  let [sliderWidth, setWidth] = useState(document.body.clientWidth);
+
+  useEffect(() => {
+    window.addEventListener('resize', setSliderWidth);
+    setWidth(document.body.clientWidth);
+  }, []);
+
+  const setSliderWidth = () => {
+    setWidth(document.body.clientWidth);
+  }
+
+  const nextPosition = () => {
+    if (currentPosition === (photoUrls.length - 1) * sliderWidth) {
+      setPosition(0);
+    } else {
+      setPosition(currentPosition += sliderWidth);
+    }
+  }
+
+  const prevPosition = () => {
+    if (currentPosition === 0) {
+      setPosition((photoUrls.length - 1) * sliderWidth);
+    } else {
+      setPosition(currentPosition -= sliderWidth);
+    }
+  }
+
   return (
     <div className='story-page'>
       <h1 className='our-story'>This is Our Story</h1>
+      <div className='slider-box'>
+        <div className='slide-btn-box right-side' onClick={nextPosition} >
+          <NextIcon className='slide-btn next' />
+        </div>
+        <div className='slide-btn-box left-side' onClick={prevPosition} >
+          <PrevIcon className='slide-btn prev' />
+        </div>
+        <div className="slider" width={photoUrls.length * sliderWidth + 'px'} style={{ right: currentPosition + 'px' }}>
+          {photoUrls.map((photo, i) => (
+            <div key={i} className='one-slide'>
+              <div className='photo-box'>
+                {photo.type === 'image' ?
+                  <img
+                    className='image-itself'
+                    aria-label='chelsey and aaron'
+                    alt='chelsey and aaron'
+                    src={photo.photoUrl}
+                  ></img>
+                  :
+                  <video
+                    controls
+                    autoPlay
+                    muted
+                    className='slide-image'
+                    aria-label='chelsey and aaron'
+                    src={photo.photoUrl}
+                    type="video/mp4"
+                  ></video>
+                }
+              </div>
+            </div>
+          ))}
+
+        </div>
+
+      </div>
+
+
+
 
       <div className='pic-div'>
 
-        <AwesomeSlider cssModule={AwesomeSliderStyles} style={{ backgroundColor: '#0c1b23' }}>
+        {/* <AwesomeSlider cssModule={AwesomeSliderStyles} style={{ backgroundColor: '#0c1b23' }}>
           {photoUrls && photoUrls.map((item, i) => (
 
             <div className='li-0' key={i}>
@@ -42,7 +111,7 @@ const OurStory = () => {
               </div>
             </div>
           ))}
-        </AwesomeSlider>
+        </AwesomeSlider> */}
       </div>
       <div className='stories'>
         <section className='story'>
