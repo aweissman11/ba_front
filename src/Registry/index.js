bcf78ae3fdd0d04e6f04be61570bede33743293d
Aaron Weissman <weissman.aaron@gmail.com>
2019-11-02 17:57:58 -0600

    reg

--- src/Registry/index.js
+++ src/Registry/index.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Registry = () => {
   return (
     <div className='events-page'>
-      <h1 className='invited'>We'd love for you to help us make our home more complete. We're just not ready to make these kinds of decisions yet!</h1>
+      <h1 className='invited'>We're just not ready to make these kinds of decisions yet!</h1>
       <div className='name-outline'>
         <div className='inner-outline'>
           <h1>Registry Info Coming Soon...</h1>
