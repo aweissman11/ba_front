d9eb5a1e5068ac36a5199d2ee81e0e274e14eacc
Aaron Weissman <weissman.aaron@gmail.com>
2019-11-10 09:32:27 -0700

    contact tab plus styling

---
+++ src/Contact/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+
+const Contact = () => {
+  return (
+    <div className='events-page'>
+      <h1 className='invited'>We're just not ready to make these kinds of decisions yet!</h1>
+      <div className='name-outline'>
+        <div className='inner-outline'>
+          <br />
+          <h1>Contact Info</h1>
+          <br />
+          <h3>Email:</h3>
+          <p><a
+            href="mailto:chelseyandaaronsbigadventure@gmail.com?subject=I%20Have%20A%20Question"
+            target='_blank'
+          >chelseyandaaronsbigadventure@gmail.com</a></p>
+          <br />
+          <h3>Phone:</h3>
+          <p>Aaron - (339) 221-1265</p>
+          <p>Chelsey - (206) 992-6861</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Contact;
--- src/Logistics/index.js
+++ src/Logistics/index.js
@@ -253,11 +253,11 @@ const Logistics = () => {
               <div className='soviet-hotel circle-pic'></div>
             </div>
             <section className='to-do-event-box'>
-              <h3 className='event-event'>NOT recommended because we hope you'll camp</h3>
+              <h3 className='event-event'>We hope as many of you as possible will camp with us</h3>
               <article className='info-article'>
                 <p className='info-detail'>Hotels</p>
-                <p className='info-block'>Hotels are not as fun as camping.</p>
-                <p className='info-block'>But, if you insist, here's some nice lodging options ten minutes away (not an actual Motel 6):</p>
+                <p className='info-block'>Hotels are not as fun as camping. But... if you're over the age of 50, or have young kids, we understand.</p>
+                <p className='info-block'>Here's some nice lodging options ten minutes away (not an actual Motel 6):</p>
                 <p className='to-do-info'>
                   <a target='_blank' rel="noopener noreferrer" href='https://www.keystoneresort.com/plan-your-trip/stay/keystone-lodging.aspx?code=keystone-lodge-hotel'>Check it out <LinkIcon /></a>
                 </p>
