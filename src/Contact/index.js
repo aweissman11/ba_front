28192fed4b9bd836a571578443e3c13a6163ef80
Aaron Weissman <weissman.aaron@gmail.com>
2019-11-10 09:40:03 -0700

    contact header

--- src/Contact/index.js
+++ src/Contact/index.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Contact = () => {
   return (
     <div className='events-page'>
-      <h1 className='invited'>We're just not ready to make these kinds of decisions yet!</h1>
+      <h1 className='invited'>Please let us know if you have any questions</h1>
       <div className='name-outline'>
         <div className='inner-outline'>
           <br />
