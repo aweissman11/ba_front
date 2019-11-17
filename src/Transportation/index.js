4c61f934aac32fd0ab47026ad1c73e19dc2df852
Aaron Weissman <weissman.aaron@gmail.com>
2019-11-17 09:35:46 -0700

    transpo

--- src/Transportation/index.js
+++ src/Transportation/index.js
@@ -13,12 +13,20 @@ const Transportation = () => {
 
       <div className='name-outline'>
         <div className='inner-outline'>
+
+          <br />
+          <h3>We have a limited number of parking spaces onsite with more a mile away from the campground.</h3>
+          <h3>If at all possible, please try to either use the Airport Shuttle or Carpool.</h3>
+          <h3>We will collect a car count at a later date.</h3>
+
+          <hr className='event-break' />
+
           <div className='info-section'>
             <div className='small-circle-div'>
               <div className='shuttle-pic circle-pic'></div>
             </div>
             <section className='to-do-event-box'>
-              <h3 className='event-event'>Transportation Options</h3>
+              <h3 className='event-event'>Airport Shuttle</h3>
               <article className='info-article'>
                 <p className='info-detail'>Shuttle Service from the Denver Airport</p>
                 <p className='info-block'>2.5 hour trip from the Airport to Dillon</p>
@@ -30,9 +38,21 @@ const Transportation = () => {
                 </p>
                 <p className='info-block'>Book in advance to secure availability!</p>
               </article>
+            </section>
+          </div>
+
+          <hr className='event-break' />
+
+          <div className='info-section flex-switch'>
+            <div className='small-circle-div'>
+              <div className='rental-cars circle-pic'></div>
+            </div>
+            <section className='to-do-event-box'>
+              <h3 className='event-event'>Rental Cars</h3>
               <article className='info-article'>
                 <p className='info-detail'>Rental Cars</p>
                 <p className='info-block'>We reccomend you rent your car from the Denver Airport</p>
+                <p className='info-block'>If you have extra seats in your car and are willing to give someone a ride, please let us know</p>
                 <p className='to-do-info'>
                   <a target='_blank' rel="noopener noreferrer" href='https://www.flydenver.com/'>Denver International <LinkIcon /></a>
                 </p>
@@ -41,6 +61,23 @@ const Transportation = () => {
           </div>
 
           <hr className='event-break' />
+
+          <div className='info-section'>
+            <div className='small-circle-div'>
+              <div className='carpool circle-pic'></div>
+            </div>
+            <section className='to-do-event-box'>
+              <h3 className='event-event'>Carpool</h3>
+              <article className='info-article'>
+                <p className='info-detail'>Make a new friend</p>
+                <p className='info-block'>If you would like to hitch a ride with someone and want help finding them, please let us know</p>
+                <p className='info-block'>We will do our best to put people in touch with other folks that are offering a spot in their cars</p>
+                <p className='to-do-info'>
+                  <a target='_blank' rel="noopener noreferrer" href='https://en.wikipedia.org/wiki/The_Hitchhiker%27s_Guide_to_the_Galaxy'>Hitchhiking <LinkIcon /></a>
+                </p>
+              </article>
+            </section>
+          </div>
 
         </div>
       </div>
